d2da64db2c57bca26a2fc6087bc11b70ac19ef1d
IP-DataVisualization-BigData-SmartHome <64257844+IP-DataVisualization-BigData-SmartHome@users.noreply.github.com>
2020-04-27 09:53:41 +0200

    Create plotly1.py
    Test für die Einführung

---
+++ 2D-Dashboard/plotly/plotly1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 09:51:22 2020
+
+@author: lukas
+"""
+
+"""
+Test-Datei für Erklärungszwecke in Umgang mit git
+"""
+
+
+a = 5
+print("Hallo")
+print("Hier wird gleich was verändert")
+print(a)
